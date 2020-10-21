b568b9ad2631eb17c4054aa6a2c75a43eff52604
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-21 13:26:58 -0500

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,10 @@
 class Song
+  
   @@count = 0
+
+  def self.count
+    @@count
+  end 
 
   attr_accessor :name, :artist, :genre
     
@@ -11,11 +16,25 @@ class Song
         @@genres << genre
         @@count += 1
     end 
+
+    def self.genre_count
+      genre_count = {}
+    @@genres.each do |genre|
+      if genre_count [genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end 
+    end 
+    genre_count
+end 
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do
+
+  end 
     
+
     
-
-      
-
-
-
 end 
