e93dd8a137ae605d1d008f464b1d0f583dae8828
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-22 13:59:16 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,19 +2,26 @@ class Song
   
   @@count = 0
 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+end 
+
+attr_accessor :name, :artist, :genre
+
+
   def self.count
     @@count
   end 
 
-  attr_accessor :name, :artist, :genre
-    
-    def initialize(name, artist, genre)
-        @name = name
-        @artist = artist
-        @@artists << artist
-        @genre = genre
-        @@genres << genre
-        @@count += 1
+  @@genres = []
+
+    def self.genres
+      @@genres.uniq
     end 
 
     def self.genre_count
@@ -29,6 +36,8 @@ class Song
     genre_count
 end 
 
+@@artists = []
+
   def self.artist_count
     @@artists.inject(Hash.new(0)) {|total, i| total[i] += 1; total}
 
@@ -37,5 +46,7 @@ end
   def self.artists
     @@artists.uniq
   end 
+
+
     
 end 
